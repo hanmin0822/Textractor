421c8228f5f56fdf25926ddfd4ccab77141b391a
QiHanmin <512240272@qq.com>
2020-03-14 10:02:43 +0800

    修改了CLI版本的输出
    将\n替换为<br>，保证游戏文本都能在一行输出内

--- GUI/host/cli.cpp
+++ GUI/host/cli.cpp
@@ -4,14 +4,26 @@
 #include <fcntl.h>
 #include <iostream>
 
+void ReplaceString(std::wstring& str, const std::wstring& src, const std::wstring& des)
+{
+	int pos = -1;
+	int curPos = 0;
+	while (-1 != (pos = str.find(src, curPos)))
+	{
+		str.replace(pos, src.size(), des);
+		curPos = pos + des.size();
+	}
+}
+
 int main()
 {
 	_setmode(_fileno(stdout), _O_U16TEXT);
-	_setmode(_fileno(stdin), _O_U16TEXT);
+	//_setmode(_fileno(stdin), _O_U16TEXT);
 	wprintf_s(L"Usage: {'attach'|'detach'|hookcode} -Pprocessid\n");
 	fflush(stdout);
 	Host::Start([](auto) {}, [](auto) {}, [](auto&) {}, [](auto&) {}, [](TextThread& thread, std::wstring& output)
 	{
+		ReplaceString(output,L"\n",L"<br>");
 		wprintf_s(L"[%I64X:%I32X:%I64X:%I64X:%I64X:%s:%s] %s\n",
 			thread.handle,
 			thread.tp.processId,
